c926445049025cdccd83db5d741363ed66142bb5
GabrielPereira146 <gabriel.brown1112@gmail.com>
2023-10-15 08:01:00 -0300

    added validation User Task

--- todolist/src/main/java/br/unesp/rc/rocketseat/todolist/task/TaskModel.java
+++ todolist/src/main/java/br/unesp/rc/rocketseat/todolist/task/TaskModel.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -32,6 +31,11 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo titulo deve ter no maximo 50 caracteres");
+        }
+        this.title = title;
+    }
 
 }
